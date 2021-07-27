4a3352e80020ec38f6eaef619ed42b8988800c53
Fatima Yousif <fyousif30@gmail.com>
2021-07-27 16:20:55 +0500

    All activities

---
+++ app/src/main/java/com/example/signupactivity/fifthbtns.java
@@ -0,0 +1,14 @@
+package com.example.signupactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fifthbtns extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fifthbtns);
+    }
+}
